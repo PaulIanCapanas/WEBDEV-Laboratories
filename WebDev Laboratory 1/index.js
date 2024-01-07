0fcfa05cebb6014e677a56c6f646ec14282feb15
PaulIanCapanas <97999879+PaulIanCapanas@users.noreply.github.com>
2024-01-07 19:08:39 +0800

    Add files via upload

---
+++ WebDev Laboratory 1/index.js
@@ -0,0 +1,31 @@
+// var manogutangs = require('sillyname');
+// var sillyname = manogutangs()
+
+// if ( manogutangs = "Saltfollower Pickle"){
+//     console.log(`"Saltfollower Pickle" your utang is Php 1000`)
+// } else if (){
+
+// }
+
+// Roadtoe Ginger 
+// Tidegargoyle Warp
+// Geodecollar Daffodil
+// Timeelk Fluff
+// Slimetooth Black
+// Clevermoth Power
+// Pinkserpent Quasar
+// Greatgorilla Sunset
+
+import manogUtangs from "sillyname";
+import { getRandomNumber } from 'random_numbers_generator';
+var sillyname = manogUtangs();
+
+
+
+const number = getRandomNumber(500,3000);
+
+console.log(`Hello! ${sillyname} your utang is Php ${number}.00`)
+
+
+
+
